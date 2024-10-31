85c37a4d484001e8a9afdde31965e74260769655
gabriela-moleta <gabriela.moleta@aluno.senai.br>
2024-10-31 10:53:08 -0300

    feat: create get by id method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -26,4 +26,29 @@ usuariosRoutes.post("/", (req, res) => {
 
 });
 
+usuariosRoutes.get("/:id", (req, res) => {
+const { id } = req.params;
+const user = usersRepository.getUserById(id);
+
+if(!user) {
+  return res.status(404).json({
+    message: `Usuário com id ${id} não encontrado!`,
+  });
+}
+
+return res.status(200).json({
+  message: `Usuario com id ${id} encontrado`,
+  user,
+});
+});
+
+usuariosRoutes.put("/:id", (req, res) => {
+
+})
+
+usuariosRoutes.delete("/:id", (req, res) => {
+
+})
+
+
 export default usuariosRoutes;
